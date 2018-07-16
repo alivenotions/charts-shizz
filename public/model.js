7dd07961a89a9982dc807d6ef2a8461311f38946
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-07-16 17:01:02 +0530

    pretty pretty

--- public/model.js
+++ public/model.js
@@ -5,7 +5,7 @@ let toggleForm = document.getElementById('toggle-form')
 let spinner = document.getElementById('spinner')
 let evaluateFeatures = document.querySelector('.evaluate-features')
 let customCheckbox = document.querySelector('.custom-checkbox')
-let ctx = document.getElementById("myChart").getContext('2d')
+let ctx = document.getElementById('myChart').getContext('2d')
 
 function submitImage(event, form) {
   event.preventDefault()
@@ -18,9 +18,9 @@ function submitImage(event, form) {
   formData.append('photo', fileField.files[0])
 
   fetch('http://localhost:9099/upload', {
-      method: 'POST',
-      body: formData
-    })
+    method: 'POST',
+    body: formData,
+  })
     .then(response => response.json())
     .then(response => {
       toggleForm.innerHTML = ''
@@ -35,7 +35,9 @@ function submitImage(event, form) {
 
 function createCustomCheckbox(key) {
   return `<div>
-    <input type="checkbox" id=${key.toLowerCase().replace(/\s+/, '')} name="option">
+    <input type="checkbox" id=${key
+      .toLowerCase()
+      .replace(/\s+/, '')} name="option">
     <label for=${key.toLowerCase().replace(/\s+/, '')}>${key}
       <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <path d="M 10 10 L 90 90"></path>
@@ -47,7 +49,10 @@ function createCustomCheckbox(key) {
 
 function readURL(input) {
   imageDisplay.src = window.URL.createObjectURL(input.files[0])
-  imageDisplay.setAttribute('style', 'display: inline-block; height: 150px; width: 150px')
+  imageDisplay.setAttribute(
+    'style',
+    'display: inline-block; height: 150px; width: 150px'
+  )
 }
 
 function createChart(labels, data) {
@@ -55,48 +60,52 @@ function createChart(labels, data) {
     type: 'horizontalBar',
     data: {
       labels: labels,
-      datasets: [{
-        label: 'Accuracy',
-        data: data,
-        backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-        borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-        borderWidth: 1
-        }]
+      datasets: [
+        {
+          label: 'Accuracy',
+          data: data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       animation: {
-        easing: 'easeOutSine'
+        easing: 'easeOutSine',
       },
       legend: {
-        display: false
+        display: false,
       },
       title: {
         display: true,
-        text: 'Accuracy model'
+        text: 'Accuracy model',
       },
       scales: {
-        xAxes: [{
-          ticks: {
-            max: 100,
-            min: 0
-          }
-        }]
-      }
-    }
+        xAxes: [
+          {
+            ticks: {
+              max: 100,
+              min: 0,
+            },
+          },
+        ],
+      },
+    },
   })
 }
 
